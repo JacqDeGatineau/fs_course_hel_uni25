edf92f8d2652727b251f77bc1909d58979834953
Henrik Heinone <henrikheinonen@outlook.com>
2025-08-13 00:20:36 +0300

    teht 1.5

--- osa 1/part1/src/App.jsx
+++ osa 1/part1/src/App.jsx
@@ -1,9 +1,9 @@
-const Header = ({course}) => {
-  console.log({course})
+const Header = ({name}) => {
+  console.log({name})
   return(
     <div>
       <h1> 
-        {course}
+        {name}
       </h1>
     </div>
   )
@@ -40,30 +40,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+    const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header name={course.name} />
 
-      <Content parts={parts} />
+      <Content parts={course.parts} />
 
-      <Total parts ={parts} />
+      <Total parts ={course.parts} />
     </div>
   )
 }
