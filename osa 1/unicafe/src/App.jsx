ea68802b5e24a0d2255be95d4fada1253f9e7ad4
Henrik Heinone <henrikheinonen@outlook.com>
2025-08-13 19:51:25 +0300

    new assignment and renaming old folder

---
+++ osa 1/unicafe/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const Statistics = ({good, neutral, bad}) => {
+  console.log({good, neutral, bad})
+  const sum = good + neutral + bad
+  console.log("sum", sum)
+  const avrg = (good + neutral + bad) / 3
+  const pos = good / sum * 100
+   
+  if (sum === 0) {
+   return(
+   <div>
+      <p>No feedback given</p>
+    </div>
+   )
+  }
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {sum}</p>
+      <p>average {avrg}</p>
+      <p>positive {pos}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const props = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+  }
+
+  const sum = Statistics(props)
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good +1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral +1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad +1)}>
+        bad
+      </button>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+    
+  )
+}
+
+export default App
